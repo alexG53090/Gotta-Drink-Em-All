31e87c79a5a848c9cb0e35263a5dc6f189e613f8
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-29 11:52:02 -0700

    fix migrations

--- migrations/20151229113834_users.js
+++ migrations/20151229113834_users.js
@@ -1,9 +1,18 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function(table){
-  table.increments();
-  table.string('email');
-  table.string('password');
-  });
+  return Promise.all([
+    knex.schema.createTable('users', function(table){
+    table.increments();
+    table.string('email');
+    table.string('password');
+    }),
+    knex.schema.createTable('beer', function(table){
+      table.increments();
+      table.string('name');
+      table.string('type');
+      table.string('brewery');
+      table.integer('rating');
+    });
+  ])
 };
 
 exports.down = function(knex, Promise) {
