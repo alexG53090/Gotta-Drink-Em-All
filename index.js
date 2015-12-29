15b53365b221cce45ea7035da4bd6da8d0207933
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-29 12:15:32 -0700

    server handlers

--- index.js
+++ index.js
@@ -8,10 +8,45 @@ var cors = require('cors');
 var auth = require('./routes/auth');
 var users = require('./routes/users');
 
-app.get('/', function(req, res, next){
-  res.send('hello')
-})
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.SECRET));
+app.use(express.static(path.join(__dirname, 'views')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(3200, function(){
   console.log('you are locked into 3200, for some reason');
 });
+
+module.exports = app;
