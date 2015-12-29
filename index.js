ed6307f5cca569b6bd32e1e2772e92cbd7d06c42
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-28 23:12:24 -0700

    server error handlers

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var Beer = function () {
 }
 
 // put in some routes --> look at CJ's examples
-// auth
-// users
+var auth = require('./routes/auth');
+var users = require('./routes/users');
 
 app.use(express.static('./views'));
 app.use(cors());
@@ -28,6 +28,37 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/', function(req, res, next){
   res.redirect('/index.html')
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(port, function(){
   console.log('Sup, dawg? You are now locked into port: ', port);
