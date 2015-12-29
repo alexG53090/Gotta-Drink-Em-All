1d1767637d620ba9f4509ce0522eaa7c551ca196
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-28 23:00:55 -0700

    make beer table

--- migrations/20151228224412_beertracker.js
+++ migrations/20151228224412_beertracker.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.createTable('beer', function(table){
+    table.increments();
+    table.string('beername');
+    table.string('beertype');
+    table.string('brewery');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.dropTable('beer')
 };
